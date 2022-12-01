6bb40e1987bb760beeb5ccaaffab14c5b3b30664
Raushan  Raj <raushan@Raushans-MacBook-Air.local>
2022-12-01 17:07:41 +0530

    Yaml Storage support added.

--- core/db.py
+++ core/db.py
@@ -1,11 +1,36 @@
+import yaml
+
 from core.settings import INSERT_CHUNK_SIZE
 
-from tinydb import TinyDB
+from tinydb import TinyDB, Storage, JSONStorage
+
+
+class YAMLStorage(Storage):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read(self):
+        with open(self.filename) as handle:
+            try:
+                data = yaml.safe_load(handle.read())
+                return data
+            except yaml.YAMLError:
+                return None
+
+    def write(self, data):
+        with open(self.filename, 'w+') as handle:
+            yaml.dump(data, handle)
+
+    def close(self):
+        pass
 
 
 class KubeDB:
-    def __init__(self, db_name, table_name):
-        self.db = TinyDB("data/" + db_name + '.json')
+    def __init__(self, db_name, table_name, format="json"):
+        if format == "yaml":
+            self.db = TinyDB("data/" + db_name + '.yaml')
+        else:
+            self.db = TinyDB("data/" + db_name + '.json', Storage=YAMLStorage)
         self.table = self.db.table(table_name)
 
     def truncate(self):
